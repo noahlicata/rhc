85fb3c132fd20cdd2fb02fccc44d27121a99492d
Noah Licata <noahinbldr@gmail.com>
2022-10-24 12:51:13 -0600

    basic page layout

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>What is health coaching?</div>
+      <div>What can it do for you?</div>
+      <div>Testimonials</div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/MyStory.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MyStory = () => {
+  return (
+    <div>
+      <div>Process of healing from sickness etc</div>
+      <div>How I found functional health</div>
+      <div>Why I decided to become a health coach</div>
+      <div>How im hoping i can help people</div>
+    </div>
+  );
+};
+
+export default MyStory;
