6df2304291bf71646bb846d41cf271e37b7efb70
Noah Licata <noahinbldr@gmail.com>
2022-10-25 16:49:35 -0600

    still working on deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2022{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
+          <a href="/" className="hover:underline">
             RRC Health Coaching™
           </a>
           . All Rights Reserved.
@@ -28,7 +28,7 @@ const Footer = () => {
             </a>
           </li>
           <li>
-            <a href="/RRCHealthCoaching/contact" className="hover:underline">
+            <a href="/contact" className="hover:underline">
               Contact
             </a>
           </li>
